5f821ae8bdc0c367da7f0235705e16f62b7d2e5f
Chenna Chandrika <84555809+chennachandrika@users.noreply.github.com>
2021-09-13 13:09:07 +0530

    bugsViewComponent

--- src/App.js
+++ src/App.js
@@ -1,52 +1,6 @@
 import "./styles.css";
-import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
-import { useDispatch, useSelector } from "react-redux";
+import BugsView from "./components/BugsView";
 
 export default function App() {
-  const bugsList = useSelector((store) => store.entities.bugs);
-  const dispatch = useDispatch();
-  let inputValue = null;
-  const onAddBug = (event) => {
-    event.preventDefault();
-    dispatch(bugAdded({ description: inputValue }));
-  };
-  const renderApp = () => {
-    console.log(bugsList);
-    return (
-      <div className="App">
-        <h1>Bugs List</h1>
-        <form onSubmit={onAddBug}>
-          <input
-            onChange={(e) => {
-              inputValue = e.target.value;
-              console.log(inputValue);
-            }}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <ul>
-          {bugsList.map((bugItem) => (
-            <li key={bugItem.id}>
-              {bugItem.description}
-              <button
-                onClick={() => {
-                  dispatch(bugRemoved({ id: bugItem.id }));
-                }}
-              >
-                X
-              </button>
-              <button
-                onClick={() => {
-                  dispatch(bugResolved({ id: bugItem.id }));
-                }}
-              >
-                Alright
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
-  return renderApp();
+  return <BugsView />;
 }
---
+++ src/components/BugsView/index.js
@@ -0,0 +1,52 @@
+import { useDispatch, useSelector } from "react-redux";
+import { bugAdded, bugRemoved, bugResolved } from "../../store/bugs";
+
+function BugsView() {
+  const bugsList = useSelector((store) => store.entities.bugs);
+  const dispatch = useDispatch();
+  let inputValue = null;
+  const onAddBug = (event) => {
+    event.preventDefault();
+    dispatch(bugAdded({ description: inputValue }));
+  };
+  const renderApp = () => {
+    return (
+      <div className="App">
+        <h1>Bugs List</h1>
+        <form onSubmit={onAddBug}>
+          <input
+            onChange={(e) => {
+              inputValue = e.target.value;
+              console.log(inputValue);
+            }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <ul>
+          {bugsList.map((bugItem) => (
+            <li key={bugItem.id}>
+              {bugItem.description}
+              <button
+                onClick={() => {
+                  dispatch(bugRemoved({ id: bugItem.id }));
+                }}
+              >
+                X
+              </button>
+              <button
+                onClick={() => {
+                  dispatch(bugResolved({ id: bugItem.id }));
+                }}
+              >
+                Alright
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  return renderApp();
+}
+
+export default BugsView;
